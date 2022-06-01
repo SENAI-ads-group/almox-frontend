52b8a71280f9946e108402d21782a0febc1a2ec4
patrickribeirorb <patrickribeiro.rb@gmail.com>
2022-05-31 23:17:03 -0300

    adiciona deploy via Github Actions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,12 @@
 export const environment = {
-    production: true,
+    production: false,
     api: {
-        baseUrl: "https://almox-fatesg-service.herokuapp.com",
+      baseUrl: 'https://almox.org/api'
     },
     auth: {
-        tokensessionStorage: "almox_access_token",
-    },
-};
+      tokensessionStorage: 'almox_access_token',
+      client_id: 'myappname123',
+      client_secret: 'myappsecret123',
+      grant_type: 'password'
+    }
+  };
