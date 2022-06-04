1523d5f6a71928dbc91f36ced18e03adae6fc1dd
patrickribeirorb <patrickribeiro.rb@gmail.com>
2022-06-04 13:14:43 -0300

    enviroment de teste

---
+++ src/environments/environment.teste.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    api: {
+        baseUrl: 'https://teste.almox.org/api'
+    },
+    auth: {
+        tokensessionStorage: 'almox_access_token',
+        client_id: 'myappname123',
+        client_secret: 'myappsecret123',
+        grant_type: 'password'
+    }
+};
